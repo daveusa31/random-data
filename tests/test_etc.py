44c94a0daa6e499f558b729ccf7dcf59253cede6
daveusa31 <bakteev2003@yandex.ru>
2020-09-03 22:21:02 +0300

    Rewrite tests on pytest

---
+++ tests/test_etc.py
@@ -0,0 +1,22 @@
+import random
+import pytest
+
+
+import random_data
+
+
+def test_russian_name_male():
+    assert isinstance(random_data.russian.names.male(), str)
+
+
+def test_russian_name_female():
+    assert isinstance(random_data.russian.names.female(), str)
+
+
+@pytest.mark.parametrize("password_length", [
+    (random.randint(10, 100))
+    for _ in range(random.randint(1, 10))
+])
+def test_length_password_10(password_length):
+    password = random_data.etc.password(length=password_length)
+    assert password_length == len(password)
